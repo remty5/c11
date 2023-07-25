bc51a5b29354417b559d78e567226991f251ba3f
rvandepu <rvandepu@student.42lehavre.fr>
2023-07-25 18:24:46 +0200

    ex01

---
+++ ex01/ft_map.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvandepu <rvandepu@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 15:27:30 by rvandepu          #+#    #+#             */
+/*   Updated: 2023/07/25 17:56:54 by rvandepu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_map(int *tab, int length, int (*f)(int))
+{
+	int	*new_tab;
+	int	i;
+
+	new_tab = malloc(length);
+	if (new_tab == NULL)
+		return (NULL);
+	i = -1;
+	while (++i < length)
+		new_tab[i] = f(tab[i]);
+	return (new_tab);
+}
